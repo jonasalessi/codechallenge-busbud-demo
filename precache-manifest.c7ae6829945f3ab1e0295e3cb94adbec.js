58880212b9a44e211f0468ef24df1153b3208817
Jonas <alessi.jonas@gmail.com>
2020-08-31 23:13:11 -0300

    Updates

---
+++ precache-manifest.c7ae6829945f3ab1e0295e3cb94adbec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a574b232a70940845c90122a676b9489",
+    "url": "/codechallenge-busbud-demo/index.html"
+  },
+  {
+    "revision": "406fb869e00f1818429d",
+    "url": "/codechallenge-busbud-demo/static/css/main.4a69ca62.chunk.css"
+  },
+  {
+    "revision": "6cd0fc67541ca4f43598",
+    "url": "/codechallenge-busbud-demo/static/js/2.6d61caee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codechallenge-busbud-demo/static/js/2.6d61caee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406fb869e00f1818429d",
+    "url": "/codechallenge-busbud-demo/static/js/main.b57edcea.chunk.js"
+  },
+  {
+    "revision": "d5a425f99c7216c6a693",
+    "url": "/codechallenge-busbud-demo/static/js/runtime-main.3e73f591.js"
+  },
+  {
+    "revision": "8131def65e114bd180cb3656cd56fc1b",
+    "url": "/codechallenge-busbud-demo/static/media/logo.8131def6.png"
+  }
+]);
